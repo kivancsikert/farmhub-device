5bd8de3c7a3d813009f4f6200cce906971d87a99
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-28 22:58:36 +0200

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+VERSION_TAG=$1
+
+if [ -z "$VERSION_TAG" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+if ! git diff --exit-code; then
+    echo "Please commit changes first"
+    exit 1
+fi
+
+if ! git diff --cached --exit-code; then
+    echo "Please commit changes first"
+    exit 1
+fi
+
+if [ $(git rev-parse --abbrev-ref HEAD) != "main" ]; then
+    echo "Can only release from 'main' branch"
+    exit 1
+fi
+
+jq ".version = \"$VERSION_TAG\"" library.json | sponge library.json
+git add library.json
+git commit -m "Prepare release $VERSION_TAG"
+
+git tag -a "$VERSION_TAG" -m "Release $VERSION_TAG"
+git push origin "$VERSION_TAG" main
