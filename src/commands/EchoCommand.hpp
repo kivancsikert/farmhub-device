b5e2869a2b90ba56ab7f932b8d944b8b62ad8a50
Lóránt Pintér <lorant.pinter@gmail.com>
2021-11-07 00:58:38 +0100

    Use new MqttHandler.publish() method in EchoCommand

--- src/commands/EchoCommand.hpp
+++ src/commands/EchoCommand.hpp
@@ -8,12 +8,11 @@ class EchoCommand {
 public:
     EchoCommand(MqttHandler& mqtt)
         : mqtt(mqtt) {
-        mqtt.registerCommand("echo", [&](const JsonObject& json) {
+        mqtt.registerCommand("echo", [&](const JsonObject& command) {
             Serial.println("Received echo command");
-            serializeJsonPretty(json, Serial);
-            DynamicJsonDocument response(json.size() + 128);
-            response["echo"] = json;
-            mqtt.publish("events/echo", response);
+            mqtt.publish("events/echo", [command](JsonObject& event) {
+                event["original"] = command;
+            });
         });
     }
 
