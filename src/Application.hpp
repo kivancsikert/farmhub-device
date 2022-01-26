c032ca83a8588d00ee4f39bc65353014e0cc345d
Lóránt Pintér <lorant.pinter@gmail.com>
2022-01-26 13:53:41 +0100

    Implement retrying of wifi connection when it's dropped

--- src/Application.hpp
+++ src/Application.hpp
@@ -128,12 +128,29 @@ private:
             appConfig.begin();
         }
 
-        WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
-            Serial.println("WiFi connected, IP address: " + WiFi.localIP().toString() + ", hostname: " + WiFi.getHostname());
-        }, SYSTEM_EVENT_STA_GOT_IP);
-        wifiProvider.begin(hostname);
         mdnsHandler.begin(hostname, name, version);
+
+        WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+            Serial.println("WiFi: connected to " + WiFi.SSID());
+        },
+            SYSTEM_EVENT_STA_CONNECTED);
+        WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+            Serial.printf("WiFi: got IP address: %s\n",
+                WiFi.localIP().toString().c_str());
+        },
+            SYSTEM_EVENT_STA_GOT_IP);
+        WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+            Serial.println("WiFi: lost IP address");
+        },
+            SYSTEM_EVENT_STA_LOST_IP);
+        WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+            Serial.println("WiFi: disconnected");
+        },
+            SYSTEM_EVENT_STA_DISCONNECTED);
+        wifiProvider.begin(hostname);
+
         otaHandler.begin(hostname);
+
         mqttHandler.begin();
         mqttHandler.publish(
             "init", [&](JsonObject& json) {
--- src/wifi/WiFiManagerProvider.hpp
+++ src/wifi/WiFiManagerProvider.hpp
@@ -2,15 +2,20 @@
 
 #include <WiFi.h>
 #include <WiFiManager.h>
+#include <chrono>
 
 #include <wifi/WiFiProvider.hpp>
+
+using namespace std::chrono;
 
 namespace farmhub { namespace client {
 
 class AbstractWiFiManagerProvider
     : public WiFiProvider {
 public:
-    AbstractWiFiManagerProvider() = default;
+    AbstractWiFiManagerProvider(seconds configurationTimeout)
+        : configurationTimeout(configurationTimeout) {
+    }
 
     virtual void begin(const String& hostname) override {
         // Explicitly set mode, ESP defaults to STA+AP
@@ -18,7 +23,7 @@ public:
 
         // Reset settings - wipe stored credentials for testing;
         // these are stored by the ESP library
-        //wm.resetSettings();
+        // wm.resetSettings();
 
         wm.setHostname(hostname.c_str());
 
@@ -26,22 +31,29 @@ public:
         // open configuration portal
         wm.setConnectTimeout(20);
 
-        // Close the configuration portal after some time and reboot
-        // if no WIFI is configured in that time
-        wm.setConfigPortalTimeout(300);
+        wm.setDebugOutput(false);
+
+        wm.setWiFiAutoReconnect(true);
     }
 
 protected:
+    const seconds configurationTimeout;
     WiFiManager wm;
 };
 
 class BlockingWiFiManagerProvider
     : public AbstractWiFiManagerProvider {
 public:
-    BlockingWiFiManagerProvider() = default;
+    BlockingWiFiManagerProvider(seconds configurationTimeout = seconds { 0 })
+        : AbstractWiFiManagerProvider(configurationTimeout) {
+    }
 
     virtual void begin(const String& hostname) override {
         AbstractWiFiManagerProvider::begin(hostname);
+
+        // Close the configuration portal after some time and reboot
+        // if no WIFI is configured in that time
+        wm.setConfigPortalTimeout(configurationTimeout.count());
 
         // Automatically connect using saved credentials,
         // if connection fails, it starts an access point
@@ -58,30 +70,64 @@ class NonBlockingWiFiManagerProvider
     : public AbstractWiFiManagerProvider,
       public BaseTask {
 public:
-    NonBlockingWiFiManagerProvider(TaskContainer& tasks)
-        : BaseTask(tasks, "WiFiManager") {
+    NonBlockingWiFiManagerProvider(TaskContainer& tasks, seconds configurationTimeout = seconds { 180 })
+        : AbstractWiFiManagerProvider(configurationTimeout)
+        , BaseTask(tasks, "WiFiManager") {};
+
+    enum class State {
+        UNINITIALIZED,
+        INITIALIZED,
+        CONFIGURING,
+        CONNECTED
     };
 
     virtual void begin(const String& hostname) override {
         AbstractWiFiManagerProvider::begin(hostname);
 
         wm.setConfigPortalBlocking(false);
-
-        //automatically connect using saved credentials if they exist
-        //If connection fails it starts an access point with the specified name
-        if (wm.autoConnect("AutoConnectAP")) {
-            Serial.println("Connected to WIFI");
-        } else {
-            Serial.println("Configportal running");
-        }
+        configHostname = hostname + "-config";
+        state = State::INITIALIZED;
     }
 
 protected:
     virtual const Schedule loop(const Timing& timing) override {
-        return wm.process()
-            ? sleepIndefinitely()
-            : sleepFor(seconds(1));
+        if (state == State::UNINITIALIZED) {
+            return sleepFor(seconds { 1 });
+        }
+        if (WiFi.isConnected()) {
+            state = State::CONNECTED;
+            // TODO Make this configurable
+            return sleepAtMost(minutes { 1 });
+        }
+
+        if (state == State::CONFIGURING) {
+            auto configurationTime = boot_clock::now() - configurationStartTime;
+            if (configurationTime < configurationTimeout) {
+                wm.process();
+                return sleepFor(milliseconds { 100 });
+            }
+            Serial.println("WiFi: configuration timed out, trying to fall back to stored configuration");
+            wm.stopConfigPortal();
+            state = State::INITIALIZED;
+        }
+
+        // automatically connect using saved credentials if they exist
+        // If connection fails it starts an access point with the specified name
+        Serial.println("WiFi: trying to connect using stored credentials");
+        if (wm.autoConnect(configHostname.c_str())) {
+            Serial.println("WiFi: connected to WIFI");
+            state = State::CONNECTED;
+        } else {
+            Serial.println("WiFi: configuration portal started, timeout: " + String((int) configurationTimeout.count()) + " seconds");
+            state = State::CONFIGURING;
+        }
+        return sleepFor(milliseconds { 100 });
     }
+
+private:
+    String configHostname;
+    State state = State::UNINITIALIZED;
+    time_point<boot_clock> configurationStartTime;
 };
 
 }}    // namespace farmhub::client
