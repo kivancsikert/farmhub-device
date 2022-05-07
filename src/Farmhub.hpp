dcb5b0efd21e2e3679f8ea67e6f14cfd9adaa045
Lóránt Pintér <lorant.pinter@gmail.com>
2022-05-07 22:34:37 +0200

    Revert back to ESP Arduino 1.x
    Arduino-ESP32 2.0.2 has some bug related to networking that prevents our code from working well.
    
    Symptoms include NTP server taking a long time to connect, and SSL connection to github.com failing upon HTTP update.

--- src/Farmhub.hpp
+++ src/Farmhub.hpp
@@ -2,6 +2,15 @@
 
 #include <Arduino.h>
 
+// Arduino 2 constants for Arduino 1 compatibility
+#ifndef ESP_ARDUINO_VERSION
+#define ARDUINO_EVENT_WIFI_STA_GOT_IP SYSTEM_EVENT_STA_GOT_IP
+#define ARDUINO_EVENT_WIFI_STA_LOST_IP SYSTEM_EVENT_STA_LOST_IP
+#define ARDUINO_EVENT_WIFI_STA_CONNECTED SYSTEM_EVENT_STA_CONNECTED
+#define ARDUINO_EVENT_WIFI_STA_DISCONNECTED SYSTEM_EVENT_STA_DISCONNECTED
+#define GPIO_NUM_NC -1
+#endif
+
 namespace farmhub { namespace client {
 
 void fatalError(String message) {
