fec46a33a30214555d5fc8f82436aef1e5ef94cf
Lóránt Pintér <lorant.pinter@gmail.com>
2022-06-15 13:13:03 +0200

    Remove useless log line

--- src/Ntp.hpp
+++ src/Ntp.hpp
@@ -51,7 +51,6 @@ public:
                 configTime(0, 0, ntpHost.c_str());
                 updateStarted = timing.loopStartTime;
 
-                Serial.println("Waiting for update... ");
                 // TODO Do this asynchrnonously once https://github.com/espressif/arduino-esp32/issues/6720 is fixed
                 while (true) {
                     if (sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED) {
